036ff5d957f5172cdbab6bfa86572497802db13b
D47R48 <ayushyadav212121@gmail.com>
2024-04-14 17:54:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "../src/component/login"
+
+function App() {
+  return (
+    <Login />
+  );
+   
+
+
+}
+
+export default App;
